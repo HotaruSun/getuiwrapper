3d2316423bae2a6a232d5b2e2a34c9fafc193aad
Sun <576759273@qq.com>
2016-09-07 15:40:55 +0800

    Test

--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -15,7 +15,7 @@
 NSString *const NotificationCategoryIdent = @"ACTIONABLE";
 NSString *const NotificationActionOneIdent = @"ACTION_ONE";
 NSString *const NotificationActionTwoIdent = @"ACTION_TWO";
-NSString *const myDevicetoken = nil;
+NSString *const myDevicetoken = @"";
 
 
 typedef enum{
